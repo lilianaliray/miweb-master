8bfea2185f422af35f2351ff2be213054cfe9757
lilianaliray <marcelo_masoka@hotmail.com>
2017-10-18 20:37:15 -0300

    url base
    para corregir la ruta base

--- application/views/eval.php
+++ application/views/eval.php
@@ -20,8 +20,7 @@
 
         <div id="div"></div>
         </div>
-      <!-- <?php //echo form_open("Welcome/validar")?>-->
-      <form>
+       <?php echo form_open("Welcome/validar")?>
 
 		<h1>Matricula</h1>
 		<fieldset id="matricula">
@@ -51,9 +50,9 @@
 		<fieldset>
 			<legend>Datos del Alumno</legend>
 				<label id="name">Nombre</label>
-				<input type="text" name="name" id="nombre">
+				<input type="text" name="nombre1" id="nombre">
 				<label>Apellido</label>
-				<input type="text" name="lastname" id="apellido">
+				<input type="text" name="apellido1" id="apellido">
 				<label>Sexo</label>
 				<input type="radio" name=""><label>Masculino</label>
 				<input type="radio" name=""><label>Femenino</label>
@@ -75,7 +74,7 @@
 		</fieldset>
 
 		</form>
-	<!--<?php //echo form_close() ?>-->
+	<?php echo form_close() ?>
 
 </body>
 </html>
